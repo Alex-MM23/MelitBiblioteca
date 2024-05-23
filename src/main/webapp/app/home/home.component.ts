51735fcd300173301a8df50c7e44022ec7e51510
Victor Morales <vm916658@gmail.com>
2024-05-23 16:15:25 +0200

    Prueba modal

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -2,31 +2,37 @@ import { Component, inject, signal, OnInit, OnDestroy } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-
+import { InicioComponent } from 'app/inicio/inicio.component';
 import SharedModule from 'app/shared/shared.module';
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/auth/account.model';
+import { ModalService } from 'app/services/modal.service';
 
 @Component({
   standalone: true,
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [SharedModule, RouterModule],
+  imports: [SharedModule, RouterModule, InicioComponent],
 })
 export default class HomeComponent implements OnInit, OnDestroy {
   account = signal<Account | null>(null);
+  modalSwitch: boolean = false;
 
   private readonly destroy$ = new Subject<void>();
 
   private accountService = inject(AccountService);
   private router = inject(Router);
 
+  constructor(private modalSS: ModalService) {}
+
   ngOnInit(): void {
     this.accountService
       .getAuthenticationState()
       .pipe(takeUntil(this.destroy$))
       .subscribe(account => this.account.set(account));
+
+    this.modalSS.$modal.subscribe(valor => (this.modalSwitch = valor));
   }
 
   login(): void {
@@ -44,5 +50,9 @@ export default class HomeComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  openModal() {
+    this.modalSwitch = true;
   }
 }
--- src/main/webapp/app/inicio/inicio.component.ts
+++ src/main/webapp/app/inicio/inicio.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/auth/account.model';
+import SharedModule from 'app/shared/shared.module';
+import { Router, RouterModule } from '@angular/router';
+import { ModalService } from 'app/services/modal.service';
 
 @Component({
   selector: 'jhi-inicio',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, RouterModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss',
 })
-export class InicioComponent {}
+export class InicioComponent implements OnInit {
+  account = inject(AccountService).trackCurrentAccount();
+
+  constructor(private modelSS: ModalService) {}
+
+  ngOnInit(): void {}
+
+  closeModal() {
+    this.modelSS.$modal.emit(false);
+  }
+}
